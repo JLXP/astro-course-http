15e10ccb4667ae300da1a3448085df81fed74663
Javier Xool <javier.xool@outlook.com>
2025-06-12 10:17:09 -0600

    se anexo cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
+  //output: "static",
+  /*adapter: node({
+    mode: "standalone",
+  }),*/
+  output: "server",
+  adapter: cloudflare(),
 });
